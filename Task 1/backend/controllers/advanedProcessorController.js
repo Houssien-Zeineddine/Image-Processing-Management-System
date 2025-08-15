a162cb78536c133a605ab00277e909137d0609ec
Houssien Zeineddine <houssienzd@hotmail.com>
2025-08-16 01:53:12 +0300

    converting image buffer to raw RGBA and 1D buffer to 2D array

--- Task 1/backend/controllers/advanedProcessorController.js
+++ Task 1/backend/controllers/advanedProcessorController.js
@@ -12,4 +12,28 @@ const advancedWorker = new Worker('advancedQueue', async job => {
     dbJob.startedAt = new Date()
     await dbJob.save()
 
+    const originalImage = dbJob.originalImageId
+
+    const { data, info } = await sharp(originalImage.fileData)
+        .raw()
+        .ensureAlpha()
+        .toBuffer({ resolveWithObject: true })
+    
+    const { width, height } = info
+
+    const pixels = []
+    for (let y = 0; y < heightl; y++) {
+        const row = []
+        for (let x = 0; x < width; x++) {
+            const idx = (y * width + x) * 4
+            row.push({
+                r: data[idx],
+                g: data[idx + 1],
+                b: data[idx + 2],
+                a: data[idx + 3]
+            })
+        }
+        pixels.push(row)
+    }
+
 })
