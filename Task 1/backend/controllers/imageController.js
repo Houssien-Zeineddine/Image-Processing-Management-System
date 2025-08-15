219b392827469ca5643de9ab62f6214d9ca8c7cd
Houssien Zeineddine <houssienzd@hotmail.com>
2025-08-15 12:48:23 +0300

    fixing bugs in upload image endpoint and testing successfully on postman

--- Task 1/backend/controllers/imageController.js
+++ Task 1/backend/controllers/imageController.js
@@ -4,19 +4,19 @@ const uploadImage = async (req, res) => {
     try {
         if (!req.file) return res.status(400).json({ error: 'No file uploaded' })
         
-        const { originalName, buffer, size } = req.file
+        const { originalname, buffer, size } = req.file
         const { name } = req.body
 
         if (!name) return res.status(400).json({ error: 'Image name is required' })
         
-        const newImage = new Image.create({
+        const newImage = await Image.create({
             name,
-            filename: originalName,
+            filename: originalname,
             fileData: buffer,
             fileSize: size
         })
 
-        res.json({ id: newImage,_id, name: newImage.name, filename: newImage.filename })
+        res.json({ id: newImage._id, name: newImage.name, filename: newImage.filename })
     } catch (error) {
         res.status(500).json({ error: error.message })
     }
