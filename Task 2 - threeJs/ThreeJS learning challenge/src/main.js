db49c169cf5983321d3f60a99e34e75ce44c570a
Houssien Zeineddine <houssienzd@hotmail.com>
2025-08-16 00:19:24 +0300

    creating a 3d sphere geometry and adding it to scene

--- Task 2 - threeJs/ThreeJS learning challenge/src/main.js
+++ Task 2 - threeJs/ThreeJS learning challenge/src/main.js
@@ -4,7 +4,7 @@ import * as THREE from 'three'
 
 const scene = new THREE.Scene()
 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth, window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg')
@@ -13,10 +13,20 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight)
 
-camera.position.setZ(10)
+camera.position.setZ(70)
+
+const geometry = new THREE.SphereGeometry( 15, 32, 16 )
+const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } )
+const sphere = new THREE.Mesh( geometry, material )
+
+scene.add( sphere )
 
 function animate() {
   requestAnimationFrame(animate)
+
+  sphere.rotation.x += 0.01
+  sphere.rotation.y += 0.005
+  sphere.rotation.z += 0.01
 
   renderer.render( scene, camera ) 
 }
