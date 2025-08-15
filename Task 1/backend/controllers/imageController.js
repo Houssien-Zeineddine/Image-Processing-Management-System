093b84878dc8af54267f833fd5283f2797b9c6f1
Houssien Zeineddine <houssienzd@hotmail.com>
2025-08-15 11:26:14 +0300

    writing image controller functions i need to implement

---
+++ Task 1/backend/controllers/imageController.js
@@ -0,0 +1,33 @@
+const Image = require('../models/Image')
+
+const uploadImage = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+const getImages = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+const getImageById = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+const deleteImage = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
