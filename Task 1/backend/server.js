3f3a7e4eac8d6c051d28fa7951a2fd32b2095094
Houssien Zeineddine <houssienzd@hotmail.com>
2025-08-15 11:15:42 +0300

    creating image model

--- Task 1/backend/server.js
+++ Task 1/backend/server.js
@@ -8,12 +8,15 @@ const app = express()
 app.use(express.json()) 
 
 //connect to DB
-mongoose.connect(process.env.MONGO_URI) 
-    .then(() => {
-        app.listen(process.env.PORT, () => { 
-            console.log('Connected to database and listening on port 4000')
-        })
-    })
-    .catch((error) => {
-        console.log(error)
-    })
+const startServer = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    app.listen(process.env.PORT, () => {
+      console.log(`Connected to database and listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+startServer();
