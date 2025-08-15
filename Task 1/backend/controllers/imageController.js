d82cf143f5955860b3c364b5b80aed2800388bcf
Houssien Zeineddine <houssienzd@hotmail.com>
2025-08-15 11:45:40 +0300

    writing upload image function in image controller

--- Task 1/backend/controllers/imageController.js
+++ Task 1/backend/controllers/imageController.js
@@ -2,9 +2,23 @@ const Image = require('../models/Image')
 
 const uploadImage = async (req, res) => {
     try {
+        if (!req.file) return res.status(400).json({ error: 'No file uploaded' })
+        
+        const { originalName, buffer, size } = req.file
+        const { name } = req.body
 
+        if (!name) return res.status(400).json({ error: 'Image name is required' })
+        
+        const newImage = new Image.create({
+            name,
+            filename: originalName,
+            fileData: buffer,
+            fileSize: size
+        })
+
+        res.json({ id: newImage,_id, name: newImage.name, filename: newImage.filename })
     } catch (error) {
-
+        res.status(500).json({ error: error.message })
     }
 }
 
