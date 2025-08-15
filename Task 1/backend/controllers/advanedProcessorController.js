d7058d8bebb10fd12eca7706210b10b9da6b8bf1
Houssien Zeineddine <houssienzd@hotmail.com>
2025-08-16 01:46:45 +0300

    fetching for job id sent from advanced queue

--- Task 1/backend/controllers/advanedProcessorController.js
+++ Task 1/backend/controllers/advanedProcessorController.js
@@ -4,5 +4,12 @@ const connection = require('../redis/connection')
 const advancedQueue = new Queue('advancedQueue', { connection })
 
 const advancedWorker = new Worker('advancedQueue', async job => {
-  
+    const { jobId } = job.data
+    const dbJob = await findById(jobId).populate('originalImageId')
+    if (!dbJob) throw new Error('Job not found')
+    
+    dbJob.status = 'processing'
+    dbJob.startedAt = new Date()
+    await dbJob.save()
+
 })
