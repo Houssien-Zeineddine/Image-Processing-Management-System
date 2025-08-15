af10475f98935f3286024acb53c51bfeea2c5eea
Houssien Zeineddine <houssienzd@hotmail.com>
2025-08-15 21:32:42 +0300

    fixing a typo in send email util

--- Task 1/backend/utils/sendEmail.js
+++ Task 1/backend/utils/sendEmail.js
@@ -8,7 +8,7 @@ const transporter = nodemailer.createTransport({
     secure: false,
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PAssword
+        pass: process.env.EMAIL_PASWORD
     },
     tls: {
         ciphers: 'SSLv3',
