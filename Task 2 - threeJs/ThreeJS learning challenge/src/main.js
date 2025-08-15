8675a31f94b339815bfae69447edc868dbd7a10f
Houssien Zeineddine <houssienzd@hotmail.com>
2025-08-16 00:29:17 +0300

    changing the color of the sphere and adding a light helper to see its position and grid helper

--- Task 2 - threeJs/ThreeJS learning challenge/src/main.js
+++ Task 2 - threeJs/ThreeJS learning challenge/src/main.js
@@ -2,10 +2,13 @@ import './style.css'
 
 import * as THREE from 'three'
 
+//scene
 const scene = new THREE.Scene()
 
+//camera
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
+//renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg')
 })
@@ -13,13 +16,27 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight)
 
-camera.position.setZ(70)
+camera.position.setZ(10)
 
-const geometry = new THREE.SphereGeometry( 15, 32, 16 )
-const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } )
+//3d geometry shape addition
+const geometry = new THREE.SphereGeometry( 2, 32, 16 )
+const material = new THREE.MeshStandardMaterial({color: 0xFF6347})
 const sphere = new THREE.Mesh( geometry, material )
 
 scene.add( sphere )
+
+//light addition
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(3, 3, 3)
+const ambientLight = new THREE.AmbientLight(0xffffff) 
+
+scene.add(pointLight, ambientLight)
+
+//add a light helper to see the position of the light
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50)
+
+scene.add(gridHelper, lightHelper)
 
 function animate() {
   requestAnimationFrame(animate)
